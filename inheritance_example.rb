9f73436b549234a8986b70ad264f051cd90a73f0
stwardak <148924249+stwardak@users.noreply.github.com>
2023-11-28 14:30:00 -0600

    added transportation class and made car & bike inherit it

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,50 +1,28 @@
 class Transportation
   def initialize
-  end
-  
-
-class Car
-  def initialize
     @speed = 0
-    @direction = 'north'
+    @direction = "north"
   end
-
   def brake
     @speed = 0
   end
-
   def accelerate
     @speed += 10
   end
-
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
+class Car < Transportation
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = 'north'
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Transportation
   def ring_bell
     puts "Ring ring!"
   end
 end
+
