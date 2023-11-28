36677d41593079635261df30308b0b8ee1e8c28b
stwardak <148924249+stwardak@users.noreply.github.com>
2023-11-28 12:30:21 -0600

    add store_item

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+item1 = {item: "scissors", color: "orange", price: 8.99}
+item2 = {item: "yarn", color: "blue", price: 6.99}
+item3 = {item: "buttons", color: "yellow", price: 3.99}
+pp item1
+
+itemA = {:item => "scissors", :color => "orange", :price => 8.99}
+itemB = {:item => "yarn", :color => "blue", :price => 6.99}
+itemC = {:item => "buttons", :color => "yellow", :price => 3.99}
+pp itemA
