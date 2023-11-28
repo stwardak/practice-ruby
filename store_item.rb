86d7b4c0a5dd9956ff10a6baae50ca829009b7c5
stwardak <148924249+stwardak@users.noreply.github.com>
2023-11-28 14:07:54 -0600

    changed input to one hash

--- store_item.rb
+++ store_item.rb
@@ -8,14 +8,30 @@
 # itemC = {:item => "buttons", :color => "yellow", :price => 3.99}
 # pp itemA
 
+# class StoreItem
+#   attr_writer :input_item, :input_color, :input_price
+#   attr_reader
+#   def initialize(input_item, input_color, input_price)
+#     @item = input_item
+#     @color = input_color
+#     @price = input_price
+#   end
+# end
+# item4 = StoreItem.new("thread", "black", "5.99")
+# pp item4
+
+
 class StoreItem
-  attr_writer :input_item, :input_color, :input_price
-  attr_reader
-  def initialize(input_item, input_color, input_price)
-    @item = input_item
-    @color = input_color
-    @price = input_price
+  attr_writer :item, :color, :price
+  attr_reader :item, :color, :price
+  def initialize(input_options)
+    @item = input_options[:item]
+    @color = input_options[:color]
+    @price = input_options[:price]
   end
 end
-item4 = StoreItem.new("thread", "black", "5.99")
+item4 = StoreItem.new(item: "thread", color: "black", price: 5.99)
 pp item4
+p item4.item
+p item4.color
+p item4.price
